a0f37395fefdc2de88da3d9cd07c4cac666f072f
duckhoi121205 <148899964+duckhoi121205@users.noreply.github.com>
2023-10-30 14:17:08 +0700

    Add files via upload

---
+++ tiết một.py
@@ -0,0 +1,17 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 10
+>>> y = 12
+>>> tong = x + y
+>>> hieu = x - y
+>>> tich = x * y
+>>> thuong = x/y
+>>> print("tong hai so",x,"+",y,"=",tong)
+tong hai so 10 + 12 = 22
+>>> print9
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print9
+NameError: name 'print9' is not defined. Did you mean: 'print'?
+>>> print("hieu hai so",x,"-",y,"=",hieu)
+hieu hai so 10 - 12 = -2
